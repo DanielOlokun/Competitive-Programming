19361a06ba9d5a155210e90b5a90923141af8750
Daniel Olokun <126188746+DanielOlokun@users.noreply.github.com>
2025-03-27 12:01:09 +0000

    Create SingleNumber.py

---
+++ arrays/SingleNumber.py
@@ -0,0 +1,9 @@
+# In a given array, every element appears twice but one. Find that one.
+# I found this q interesting, due to the use of XOR (^=) usually used in Logic/ Discrete Maths.
+# One to note
+
+def singleNumber(self, nums):
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
