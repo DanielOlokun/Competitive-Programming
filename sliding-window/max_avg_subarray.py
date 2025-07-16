71e3cb17e47b6337e47da72f8926ba7b6033e399
Daniel Olokun <126188746+DanielOlokun@users.noreply.github.com>
2025-07-16 13:01:59 +0100

    Create max_avg_subarray.py

---
+++ sliding-window/max_avg_subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+
+        curr_sum = max_sum = sum(nums[:k])    # inital windows -- > current sum starts off as max sum which is the sum of nums from start of array to k ( as per q )
+
+        for i in range(k, len(nums)):
+            curr_sum += nums[i] - nums[i - k]    # this slides the window -- > adds the next number of and removes the first one -- > efficiently -- > O(1) time
+            max_sum = max(max_sum, curr_sum)    # updates max if needed
+
+
+        return max_sum / float(k)  # ensures floating point as per q
