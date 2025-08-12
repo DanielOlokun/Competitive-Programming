7a9aa8db1b4ab9ac6066b79fdc75c5857301c46e
Daniel Olokun <126188746+DanielOlokun@users.noreply.github.com>
2025-08-12 19:02:31 +0100

    Update max-depth-binary-tree.py

--- graphs/dfs/max-depth-binary-tree.py
+++ graphs/dfs/max-depth-binary-tree.py
@@ -17,4 +17,30 @@ class Solution(object):
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+#2 -----> ITERATIVE WAY
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        max_depth = 0
+        stack = [(root, 1)]  # Store (node, depth)
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                max_depth = max(max_depth, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return max_depth
         
